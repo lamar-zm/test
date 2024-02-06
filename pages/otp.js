b5d95d44045554fee3e3433aec4f6003dadb33c9
lamar-zm <okhalifa22@yahoo.com>
2024-02-06 13:50:51 +0300

    minor changes in login page

--- pages/otp.js
+++ pages/otp.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
-import loginIllustration from "../public/assests/loginIllustration.svg";
-import logo from '../public/assests/colourdLogo.svg'
-import whatsapp from '../public/assests/whatsapp.svg'
+import loginIllustration from "../public/assests/login2.svg";
+import ColourdLogo from '../public/assests/colourdLogo.svg'
 import blackAndWhiteLogo from '../public/assests/blackandwhitelogo.svg'
 
 function Home() {
   return (
     <>
-      <div className="h-screen flex flex-col justify-between bg-[#F5F7F8]">
+      <div className="h-screen flex flex-col justify-between">
         <div className="h-[12%] flex items-center justify-between p-12">
           <div>
             <button className="text-2xl bg-gradient-to-r from-pink-500 to-purple-600 text-white py-6 px-6 rounded-xl">
@@ -16,44 +15,44 @@ function Home() {
           </div>
           <div className="w-[6%]">
             <Image
-              src={logo}
+              src={ColourdLogo}
               alt="Logo"
             />
           </div>
         </div>
         <div className="h-[80%] w-full flex items-center justify-center">
-          <div className="h-[90%] w-[80%] flex items-center rounded-xl bg-white">
+          <div className="h-[90%] w-[80%] flex items-center rounded-xl">
             <div className="h-full w-[50%] flex relative">
               <Image
-              className="absolute h w-[0%] right-[-20px]"
                 src={loginIllustration}
                 alt="Login Illustration"
                 layout="fill"
+                objectFit="cover"
               />
               <div className="bg-[#E8F2F9] w-[60%]"></div>
             </div>
-            <div className="h-[85%] w-[50%] flex items-center justify-center px-8">
-              <div className="h-[80%] w-full  p-3 flex flex-col items-end justify-between">
+            <div className="h-[85%] w-[50%] flex items-center justify-center">
+              <div className="h-[80%] w-full  p-3 flex flex-col items-end justify-evenly">
                 <div className="flex flex-col gap-6 items-end">
-                  <p className="text-[#142457] text-5xl font-semibold">تسجيل الدخول</p>
-                  <p className="text-[#c7c7c7] text-2xl">
+                  <p className="text-[#142457] text-5xl">تسجيل الدخول</p>
+                  <p className="text-[#c7c7c7] text-xl font-semibold">
                     الرجاء ادخال رقم هاتفك المحمول
                   </p>
                 </div>
-                <div className="h-[40%] w-full flex flex-col justify-center gap-8">
-                  <div className="flex items-center gap-2">
-                    <div className="relative h-[80%] flex items-center justify-end w-full">
+                <div className="h-[40%] flex flex-col justify-center gap-8">
+                  <div className="flex gap-2">
+                    <div className="relative">
                       <input
-                        className="border bg-transparent h-full w-[80%] px-3 text-xl rounded-lg outline-none relative"
+                        className="border bg-transparent py-9 px-3 w-[449px] text-xl rounded-lg outline-none relative"
                         type="tel"
                         name="phone"
                         id="phone-input"
                         dir="rtl"
                         placeholder="ادخل رقم الهاتف"
                       />
-                      <span className="right-3 top-1 absolute text-sm  ">رقم الهاتف</span>
+                      <span className="right-3 top-1 absolute text-lg font-semibold ">رقم الهاتف</span>
                     </div>
-                    <select className="py-8 rounded-lg outline-none border">
+                    <select className="py-6 rounded-lg outline-none border">
                       <option value="whatsapp">
                       </option>
                     </select>
